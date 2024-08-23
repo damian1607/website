dc1bc0cc8d7cffa0858ad7380b9f3e5af3c92fd2
Damian <107065553+damian1607@users.noreply.github.com>
2024-08-24 01:27:31 +0200

    pfad fix zu root folder

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
 function loadHTML(elementId, file) {
     fetch(file)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-            return response.text();
-        })
-        .then(data => document.getElementById(elementId).innerHTML = data)
-        .catch(error => console.error('Error loading HTML:', error));
+        .then(response => response.text())
+        .then(data => document.getElementById(elementId).innerHTML = data);
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    loadHTML('header', '../header.html');
-    loadHTML('footer', '../footer.html');
+    loadHTML('header', '/header.html');
+    loadHTML('footer', '/footer.html');
 });
