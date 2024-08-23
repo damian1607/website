f06cca7c605d3441fa987690045c3d02b083071f
Damian <107065553+damian1607@users.noreply.github.com>
2024-08-24 01:15:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -11,6 +11,6 @@ function loadHTML(elementId, file) {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    loadHTML('header', '/header.html');
-    loadHTML('footer', '/footer.html');
+    loadHTML('header', '../header.html');
+    loadHTML('footer', '../footer.html');
 });
