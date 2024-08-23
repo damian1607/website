20b216f1b98a30e90c6e9d9245a1fe5ed08c77dc
Damian <107065553+damian1607@users.noreply.github.com>
2024-08-24 01:13:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 function loadHTML(elementId, file) {
     fetch(file)
-        .then(response => response.text())
-        .then(data => document.getElementById(elementId).innerHTML = data);
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.text();
+        })
+        .then(data => document.getElementById(elementId).innerHTML = data)
+        .catch(error => console.error('Error loading HTML:', error));
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    loadHTML('header', 'header.html');
-    loadHTML('footer', 'footer.html');
+    loadHTML('header', '/header.html');
+    loadHTML('footer', '/footer.html');
 });
